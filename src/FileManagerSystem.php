64196f105ac584154bf644ab6ee29ac1201eb5f7
Julien <90765278+Anfallnorr@users.noreply.github.com>
2025-02-07 07:43:28 +0100

    Update FileManagerSystem.php

--- src/FileManagerSystem.php
+++ src/FileManagerSystem.php
@@ -4,9 +4,14 @@ namespace Anfallnorr\FileManagerSystem;
 
 use Symfony\Component\HttpKernel\Bundle\Bundle;
 
-class FileManagerSystem extends Bundle
+final class FileManagerSystem extends Bundle
 {
     // Vous pouvez personnaliser ici des comportements spécifiques du bundle.
+    /* public function getContainerExtension(): ?FileManagerSystemExtension
+    {
+        return new FileManagerSystemExtension();
+    } */
+    
     public function getPath(): string
     {
         return \dirname(__DIR__);
