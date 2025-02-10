f570888593347d6a36e0b034fcfa144ad2ab99da
Julien <90765278+Anfallnorr@users.noreply.github.com>
2025-02-10 07:36:20 +0100

    Update FileManagerController.php

--- src/Controller/FileManagerController.php
+++ src/Controller/FileManagerController.php
@@ -2,56 +2,90 @@
 
 namespace Anfallnorr\FileManagerSystem\Controller;
 
+use Anfallnorr\FileManagerSystem\Form\CreateFolderType;
+use Anfallnorr\FileManagerSystem\Form\MoveFileType;
+use Anfallnorr\FileManagerSystem\Form\RenameFileType;
+use Anfallnorr\FileManagerSystem\Form\UploadFileType;
+use Anfallnorr\FileManagerSystem\Service\FileManagerService;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
-use Symfony\Component\Filesystem\Filesystem;
 use Symfony\Component\HttpFoundation\File\Exception\FileException;
-use Anfallnorr\FileManagerSystem\Form\CreateFolderType;
-use Anfallnorr\FileManagerSystem\Form\UploadFileType;
-use Anfallnorr\FileManagerSystem\Form\RenameFileType;
-use Anfallnorr\FileManagerSystem\Form\MoveFileType;
+use Symfony\Contracts\Translation\TranslatorInterface;
 
 class FileManagerController extends AbstractController
 {
-    #[Route('/file-manager', name: 'file_manager')]
-    public function index(Request $request, Filesystem $filesystem): Response
-    {
-        $defaultDirectory = $this->getParameter('file_manager_system.default_directory');
+	public function __construct(
+		private FileManagerService $fileManagerService,
+		private TranslatorInterface $translator
+	) {}
 
-        // Création de dossier
-        $createFolderForm = $this->createForm(CreateFolderType::class);
-        $createFolderForm->handleRequest($request);
-        if ($createFolderForm->isSubmitted() && $createFolderForm->isValid()) {
-            $folderName = $createFolderForm->get('folderName')->getData();
-            $folderPath = $defaultDirectory . '/' . $folderName;
+	#[Route('/file-manager', name: 'file_manager')]
+	public function index(Request $request): Response
+	{
+		$fmService = $this->fileManagerService;
+		$fmService->setDefaultDirectory('/var/uploads'); // Example for personnal folder space: '/var/uploads/' . $his->getUser()->getId()
+		// $directories = $fmService->getDirs();
+		// $files = $fmService->getFiles();
+		
 
-            if (!$filesystem->exists($folderPath)) {
-                $filesystem->mkdir($folderPath);
-                $this->addFlash('success', 'Dossier créé avec succès.');
-            }
-        }
+		// Création de dossier
+		$createFolderForm = $this->createForm(CreateFolderType::class);
+		$createFolderForm->handleRequest($request);
+		
+		if ($createFolderForm->isSubmitted() && $createFolderForm->isValid()) {
 
-        // Upload de fichier
-        $uploadFileForm = $this->createForm(UploadFileType::class);
-        $uploadFileForm->handleRequest($request);
-        if ($uploadFileForm->isSubmitted() && $uploadFileForm->isValid()) {
-            $file = $uploadFileForm->get('file')->getData();
-            if ($file) {
-                $fileName = $file->getClientOriginalName();
-                try {
-                    $file->move($defaultDirectory, $fileName);
-                    $this->addFlash('success', 'Fichier uploadé avec succès.');
-                } catch (FileException $e) {
-                    $this->addFlash('error', 'Erreur lors de l’upload.');
-                }
-            }
-        }
+			$folderName = $createFolderForm->get('folderName')->getData();
 
-        return $this->render('@FileManagerSystem/index.html.twig', [
-            'createFolderForm' => $createFolderForm->createView(),
-            'uploadFileForm' => $uploadFileForm->createView(),
-        ]);
-    }
+			if (!$fmService->exists($folderName)) {
+				$fmService->createDir($folderName);
+
+				$this->addFlash(
+					'success',
+					$this->translator->trans('file_manager.folder_created_successfully')
+				);
+			} else {
+				$this->addFlash(
+					'warning',
+					$this->translator->trans('file_manager.the_folder_already_exists', ['%foldername%' => $folderName])
+				);
+			}
+
+			return $this->redirectToRoute('app_home');
+		}
+
+
+		// Upload de fichier
+		$uploadFileForm = $this->createForm(UploadFileType::class);
+		$uploadFileForm->handleRequest($request);
+
+		if ($uploadFileForm->isSubmitted() && $uploadFileForm->isValid()) {
+			$files = $uploadFileForm->get('file')->getData();
+
+			if ($files) {
+				try {
+					$uploaded = $fmService->upload($files, $fmService->getDefaultDirectory(), false);
+
+					$this->addFlash(
+						'success',
+						$this->translator->trans('file_manager.file_uploaded_successfully')
+					);
+				} catch (FileException $e) {
+					$this->addFlash(
+						'danger',
+						$this->translator->trans('file_manager.error_while_uploading', ['%message%' => $e])
+					);
+				}
+			}
+
+			return $this->redirectToRoute('app_home');
+		}
+
+
+		return $this->render('@FileManagerSystem/index.html.twig', [
+			'createFolderForm' => $createFolderForm->createView(),
+			'uploadFileForm' => $uploadFileForm->createView(),
+		]);
+	}
 }
