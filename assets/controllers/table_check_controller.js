accce56010be83b7df27b088c00e333ffc7d7979
Julien <90765278+Anfallnorr@users.noreply.github.com>
2025-02-24 14:30:29 +0100

    Add files via upload

---
+++ assets/controllers/table_check_controller.js
@@ -0,0 +1,75 @@
+import { Controller } from "@hotwired/stimulus";
+
+export default class extends Controller {
+	static targets = ["rowInputs", "itemDelete", "itemDirDelete"];
+	static values = {
+		check: Array,
+		checkDir: Array
+	}
+
+	connect()
+	{
+		console.log("Contrôleur table-check connecté !");
+		// this.checkValue = []; // Initialise le tableau
+	}
+
+	changes(event) {
+		// console.log(event.target.closest('table'));
+		this.checks(event.target.checked);
+	}
+
+	checks = (value = false) => {
+		let check = [];
+
+		this.rowInputsTargets.forEach(input => {
+			input.checked = value;
+
+			if (value) {
+				check.push(input.value);
+			}
+		});
+
+		this.checkValue = check; // Met à jour la valeur réactive
+
+		this.updateHiddenInput();
+		// console.log(this.checkValue);
+	}
+
+	check(event) {
+		const rowValue = event.target.value;
+
+		if (event.target.checked) {
+			this.checkValue = [...this.checkValue, rowValue]; // Ajout
+		} else {
+			this.checkValue = this.checkValue.filter(v => v !== rowValue); // Suppression
+		}
+
+		this.updateHiddenInput();
+		// console.log(this.checkValue);
+	}
+
+	updateHiddenInput() {
+		if (this.hasItemDeleteTarget) {
+			this.itemDeleteTarget.value = JSON.stringify(this.checkValue);
+		}
+	}
+
+	checkDir(event) {
+		const rowValue = event.target.value;
+
+		if (event.target.checked) {
+			this.checkDirValue = [...this.checkDirValue, rowValue]; // Ajout
+		} else {
+			this.checkDirValue = this.checkDirValue.filter(v => v !== rowValue); // Suppression
+		}
+
+		this.updateHiddenDirInput();
+		// console.log(this.checkDirValue);
+	}
+
+	updateHiddenDirInput() {
+		if (this.hasItemDirDeleteTarget) {
+			this.itemDirDeleteTarget.value = JSON.stringify(this.checkDirValue);
+		}
+	}
+}
