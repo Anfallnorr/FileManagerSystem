87158019bb7202371a267561ffbdd4550155c61c
Julien <90765278+Anfallnorr@users.noreply.github.com>
2025-11-21 10:46:43 +0100

    Refactor UploadFileType to use named arguments

--- src/Form/UploadFileType.php
+++ src/Form/UploadFileType.php
@@ -19,8 +19,8 @@ class UploadFileType extends AbstractType
 	public function buildForm(FormBuilderInterface $builder, array $options): void
 	{
 		$builder
-			->add('file', FileType::class, [
-				'label' => new TranslatableMessage('file_manager.select_a_file'),
+			->add(child: 'file', type: FileType::class, options: [
+				'label' => new TranslatableMessage(message: 'file_manager.select_a_file'),
 				'mapped' => false,
 				'multiple' => true,
 				'required' => true,
@@ -29,12 +29,12 @@ class UploadFileType extends AbstractType
 				], */
 				'attr' => [
 					'class' => 'dropzone-field',
-					'placeholder' => new TranslatableMessage('file_manager.drag_and_drop_or_browse'),
+					'placeholder' => new TranslatableMessage(message: 'file_manager.drag_and_drop_or_browse'),
 					'data-user' => $options['user'], // id user
 					'data-url' => $options['route'], // generateUrl with params
 					'data-current-folder' => $options['current_folder'], // {folder} route param
-					'data-cancel-label' => $this->translator->trans('file_manager.cancel'),
-					'data-remove-label' => $this->translator->trans('file_manager.clear'),
+					'data-cancel-label' => $this->translator->trans(id: 'file_manager.cancel'),
+					'data-remove-label' => $this->translator->trans(id: 'file_manager.clear'),
 					'data-max-size-folder' => 100, // Personal folder limit to avoid overload in percentage
 					'data-max-filesize' => 2, // PHP upload_max_filesize param
 					'data-max-files' => 20, // PHP max_file_uploads param
@@ -45,15 +45,15 @@ class UploadFileType extends AbstractType
             /* ->add('rootPath', HiddenType::class, [
                 'data' => base64_encode($options['root_path']), // $this->getParameter('kernel.project_dir')
             ]) */
-			->add('submit', SubmitType::class, [
-				'label' => new TranslatableMessage('file_manager.send')
+			->add(child: 'submit', type: SubmitType::class, options: [
+				'label' => new TranslatableMessage(message: 'file_manager.send')
 			]);
 	}
 
 	public function configureOptions(OptionsResolver $resolver): void
 	{
 		// $resolver->setDefaults([]);
-		$resolver->setDefaults([
+		$resolver->setDefaults(defaults: [
 			// 'data_class' => null,
 			'user' => null, // User parameter for route: userId
 			'route' => null, // Route to the dropzone form, generateUrl with params
