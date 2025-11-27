28fa62ead2ce60af57f9f3574bb9abd7eb273491
Julien <90765278+Anfallnorr@users.noreply.github.com>
2025-11-27 05:59:53 +0100

    Update UploadFileType.php

--- src/Form/UploadFileType.php
+++ src/Form/UploadFileType.php
@@ -30,20 +30,20 @@ class UploadFileType extends AbstractType
 				'attr' => [
 					'class' => 'dropzone-field',
 					'placeholder' => new TranslatableMessage(message: 'file_manager.drag_and_drop_or_browse'),
-					'data-user' => $options['user'], // id user
-					'data-url' => $options['route'], // generateUrl with params
+					'data-user' => $options['user'],                     // id user
+					'data-url' => $options['route'],                     // generateUrl with params
 					'data-current-folder' => $options['current_folder'], // {folder} route param
 					'data-cancel-label' => $this->translator->trans(id: 'file_manager.cancel'),
 					'data-remove-label' => $this->translator->trans(id: 'file_manager.clear'),
-					'data-max-size-folder' => 100, // Personal folder limit to avoid overload in percentage
-					'data-max-filesize' => 2, // PHP upload_max_filesize param
-					'data-max-files' => 20, // PHP max_file_uploads param
-					'data-param-name' => 'file_manager_system', // Override for Dropzone JS
-					'data-dropzone-target' => 'dropzoneField' // For Stimulus Dropzone JS
+					'data-max-size-folder' => 100,                       // Personal folder limit to avoid overload in percentage
+					'data-max-filesize' => 2,                            // PHP upload_max_filesize param
+					'data-max-files' => 20,                              // PHP max_file_uploads param
+					'data-param-name' => 'file_manager_system',          // Override for Dropzone JS
+					'data-dropzone-target' => 'dropzoneField'            // For Stimulus Dropzone JS
 				],
 			])
             /* ->add('rootPath', HiddenType::class, [
-                'data' => base64_encode($options['root_path']), // $this->getParameter('kernel.project_dir')
+                'data' => base64_encode($options['root_path']),          // $this->getParameter('kernel.project_dir')
             ]) */
 			->add(child: 'submit', type: SubmitType::class, options: [
 				'label' => new TranslatableMessage(message: 'file_manager.send')
@@ -55,10 +55,10 @@ class UploadFileType extends AbstractType
 		// $resolver->setDefaults([]);
 		$resolver->setDefaults(defaults: [
 			// 'data_class' => null,
-			'user' => null, // User parameter for route: userId
-			'route' => null, // Route to the dropzone form, generateUrl with params
+			'user' => null,           // User parameter for route: userId
+			'route' => null,          // Route to the dropzone form, generateUrl with params
 			'current_folder' => null, // {folder} route param
-			// 'root_path' => null, // $this->getParameter('kernel.project_dir')
+			// 'root_path' => null,   // $this->getParameter('kernel.project_dir')
 		]);
 	}
 }
