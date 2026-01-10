fe922d226c289ef801546c3731bf32bc85394faa
Julien <90765278+Anfallnorr@users.noreply.github.com>
2026-01-10 06:13:43 +0100

    Update UploadFileType.php

--- src/Form/UploadFileType.php
+++ src/Form/UploadFileType.php
@@ -30,16 +30,18 @@ class UploadFileType extends AbstractType
 				'attr' => [
 					'class' => 'dropzone-field',
 					'placeholder' => new TranslatableMessage(message: 'file_manager.drag_and_drop_or_browse'),
-					'data-user' => $options['user'],                     // id user
-					'data-url' => $options['route'],                     // generateUrl with params
-					'data-current-folder' => $options['current_folder'], // {folder} route param
+					'data-user' => $options['user'],                      // (required) id user
+					'data-url' => $options['route'],                      // (required) generateUrl with params
+					'data-max-filesize' => $options['max_filesize'] ?? 2, // Override Dropzone maxFilesize (PHP upload_max_filesize) param
+					'data-max-files' => $options['max_files'] ?? 20,      // Override Dropzone maxFiles (PHP max_file_uploads) param
+					'data-auto-process' => $options['auto_process'],      // Override Dropzone autoProcessQueue param
+					'data-multiple' => $options['multiple'],              // Override Dropzone uploadMultiple param
+					'data-current-folder' => $options['current_folder'],  // {folder} route param
 					'data-cancel-label' => $this->translator->trans(id: 'file_manager.cancel'),
 					'data-remove-label' => $this->translator->trans(id: 'file_manager.clear'),
-					'data-max-size-folder' => 100,                       // Personal folder limit to avoid overload in percentage
-					'data-max-filesize' => 2,                            // PHP upload_max_filesize param
-					'data-max-files' => 20,                              // PHP max_file_uploads param
-					'data-param-name' => 'file_manager_system',          // Override for Dropzone JS
-					'data-dropzone-target' => 'dropzoneField'            // For Stimulus Dropzone JS
+					'data-max-size-folder' => 100,                        // Personal folder limit to avoid overload in percentage
+					'data-param-name' => 'file_manager_system',           // (required) Override for Dropzone JS 
+					'data-dropzone-target' => 'dropzoneField'             // (required) For Stimulus Dropzone JS
 				],
 			])
             /* ->add('rootPath', HiddenType::class, [
@@ -52,13 +54,17 @@ class UploadFileType extends AbstractType
 
 	public function configureOptions(OptionsResolver $resolver): void
 	{
-		// $resolver->setDefaults([]);
 		$resolver->setDefaults(defaults: [
 			// 'data_class' => null,
-			'user' => null,           // User parameter for route: userId
-			'route' => null,          // Route to the dropzone form, generateUrl with params
+			'user' => null,           // (required) User parameter for route: userId
+			'route' => null,          // (required) Route to the dropzone form, generateUrl with params
+			'max_filesize' => null,   // Override Dropzone maxFilesize param
+			'max_files' => null,      // Override Dropzone maxFiles param
+			'auto_process' => null,   // Override Dropzone autoProcessQueue param
+			'multiple' => null,       // Override Dropzone uploadMultiple param
 			'current_folder' => null, // {folder} route param
 			// 'root_path' => null,   // $this->getParameter('kernel.project_dir')
 		]);
 	}
 }
+
