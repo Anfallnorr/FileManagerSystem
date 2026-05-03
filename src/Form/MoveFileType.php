de2d8a80e11c3dbf3a229e28b745257680a6b185
Julien <90765278+Anfallnorr@users.noreply.github.com>
2026-05-03 07:14:48 +0200

    Update MoveFileType.php

--- src/Form/MoveFileType.php
+++ src/Form/MoveFileType.php
@@ -3,11 +3,12 @@
 namespace Anfallnorr\FileManagerSystem\Form;
 
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
+use Symfony\Component\Form\Extension\Core\Type\HiddenType;
+// use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
-use Symfony\Component\Form\Extension\Core\Type\TextType;
-use Symfony\Component\Form\Extension\Core\Type\SubmitType;
-use Symfony\Component\Form\Extension\Core\Type\HiddenType;
 use Symfony\Component\Translation\TranslatableMessage;
 
 class MoveFileType extends AbstractType
@@ -20,18 +21,39 @@ class MoveFileType extends AbstractType
 				'data' => $options['current_folder']
 				// 'data' => '/toto'
 			])
-			->add(child: 'newPath', type: TextType::class, options: [
-				'label' => new TranslatableMessage(message: 'file_manager.new_path')
+			// ->add(child: 'newPath', type: TextType::class, options: [
+			->add(child: 'newPath', type: ChoiceType::class, options: [
+				'label' => new TranslatableMessage(message: 'file_manager.new_path'),
+				'choices' => $options['folder_list']
 			])
 			->add(child: 'submit', type: SubmitType::class, options: [
 				'label' => new TranslatableMessage(message: 'file_manager.move')
-			]);
+			])
+		;
+
+		$submitOptions = [
+			'label' => new TranslatableMessage(message: 'file_manager.move'),
+			'attr' => ['class' => $options['submit_class']],
+		];
+
+		if (!empty($options['submit_icon'])) {
+			$submitOptions['icon_before'] = $options['submit_icon'];
+		}
+
+		$builder->add(
+			child: 'submit',
+			type: SubmitType::class,
+			options: $submitOptions
+		);
 	}
 
 	public function configureOptions(OptionsResolver $resolver): void
 	{
 		$resolver->setDefaults(defaults: [
 			'current_folder' => null,
+			'folder_list' => null,
+			'submit_class' => 'btn-primary rounded-pill px-4',
+			'submit_icon' => null
 		]);
 	}
 }
